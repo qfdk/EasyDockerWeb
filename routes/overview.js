48f633bd060a0b83138dfeb84c671cea9820edab
qfdk <qfdk2010@gmail.com>
2018-03-20 20:46:23 +0100

    [Fix] message error

--- routes/overview.js
+++ routes/overview.js
@@ -5,6 +5,11 @@ var docker = new Docker();
 /* GET home page. */
 router.get('/', function (req, res, next) {
   docker.info(function (err, info) {
+    if (err) {
+      res.render('error', {
+        message: "Docker is running ?"
+      });
+    }
     res.render('overview', {
       info: info
     });
